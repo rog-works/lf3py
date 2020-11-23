6aad9ba581338739c10dafd18b63a05060868417
rog-works <rog_works@yahoo.co.jp>
2020-11-24 08:15:20 +0900

    refactor: changed to format_exception

--- framework/lang/error.py
+++ framework/lang/error.py
@@ -3,4 +3,4 @@ from typing import List
 
 
 def stacktrace(error: Exception) -> List[str]:
-    return traceback.format_tb(error.__traceback__)
+    return traceback.format_exception(type(error), error, error.__traceback__)
