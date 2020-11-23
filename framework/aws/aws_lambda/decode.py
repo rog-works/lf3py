e580618c6187b0e2545e7cfb895d02f901ad43de
rog-works <rog_works@yahoo.co.jp>
2020-11-23 15:55:29 +0900

    fix: decode body

--- framework/aws/aws_lambda/decode.py
+++ framework/aws/aws_lambda/decode.py
@@ -1,3 +1,8 @@
+from base64 import b64decode
+import json
+from urllib.parse import parse_qsl
+
+
 from framework.http.data import Request
 
 
@@ -6,5 +11,15 @@ def decode_request(event: dict) -> Request:
         path=event['path'],
         method=event['httpMethod'],
         headers=event['headers'],
-        params={**event['queryStringParameters'], **event['body']},
+        params={**event['queryStringParameters'], **decode_body(event)},
     )
+
+
+def decode_body(event: dict) -> dict:
+    content_type = event['headers'].get('content-type', '')
+    if content_type.find('application/json') != -1:
+        return json.loads(event['body'])
+    elif content_type.find('application/x-www-form-urlencoded') != -1:
+        return dict(parse_qsl(b64decode(event['body']).decode('utf-8')))
+    else:
+        return {}
