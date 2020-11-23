8e237ba0368578b55f12b05df3b1a1eaa90fbdc9
rog-works <rog_works@yahoo.co.jp>
2020-11-23 16:54:04 +0900

    test: add i18n/translator

---
+++ tests/framework/i18n/test_translator.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from framework.i18n.translator import Translator
+from tests.helper.fixture import data_provider
+
+
+class TestTranslator(TestCase):
+    CONFIG = {
+        'http': {
+            '500': '500 Internal Server Error',
+        },
+    }
+
+    @data_provider([
+        ('http.500', '500 Internal Server Error'),
+    ])
+    def test_trans(self, path: str, expected: str):
+        translator = Translator(self.CONFIG)
+        self.assertEquals(translator.trans(path), expected)
