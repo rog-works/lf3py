51a707f3ea6be5673ef5e242d362f3f18f37a0a4
rog-works <rog_works@yahoo.co.jp>
2020-11-25 08:22:56 +0900

    test: add api/router

---
+++ tests/unit/framework/api/test_router.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from framework.api.router import Router
+from framework.task.result import Result
+
+
+def runner_a() -> Result:
+    return Result()
+
+
+def runner_b() -> Result:
+    return Result()
+
+
+class TestRouter(TestCase):
+    def test_resolve(self):
+        router = Router({
+            'GET /models': lambda: runner_a,
+            r'GET /models/\d+': lambda: runner_b,
+        })
+        self.assertEqual(router.resolve('GET', '/models'), runner_a)
+        self.assertEqual(router.resolve('GET', '/models/1234'), runner_b)
