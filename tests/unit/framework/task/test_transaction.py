7171a2ca1e77a18fb2feb7f40a042a8065b5074a
rog-works <rog_works@yahoo.co.jp>
2020-11-24 23:06:59 +0900

    test: add task/transaction

---
+++ tests/unit/framework/task/test_transaction.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+
+from framework.task.transaction import transaction
+
+
+def error_func():
+    e = False  # noqa: F841
+    f = 0.0  # noqa: F841
+    raise ValueError()
+
+
+class TestTransation(TestCase):
+    def test_transaction(self):
+        try:
+            a = 1  # noqa: F841
+            b = 'hoge'  # noqa: F841
+            self.transaction_func()
+        except ValueError:
+            pass
+
+    def transaction_func(self):
+        with transaction(error_handler=self.error_handler):
+            c = True  # noqa: F841
+            d = 1.0  # noqa: F841
+            error_func()
+
+    def error_handler(self, error: BaseException, context: dict):
+        self.assertEqual(type(error), ValueError)
+        self.assertEqual(context.get('a'), None)
+        self.assertEqual(context.get('b'), None)
+        self.assertEqual(context['c'], True)
+        self.assertEqual(context['d'], 1.0)
+        self.assertEqual(context.get('e'), None)
+        self.assertEqual(context.get('f'), None)
