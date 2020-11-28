56ab0950970b3311dc4ddf0c5ce5bec89dd1b86b
rog-works <rog_works@yahoo.co.jp>
2020-11-28 17:57:51 +0900

    test: add example/controllers/models show action

--- tests/e2e/example/controllers/test_models.py
+++ tests/e2e/example/controllers/test_models.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from example.provider import aws_app
+from framework.lang.module import unload_module
 from tests.helper.fixture import data_provider
 
 
@@ -18,9 +19,23 @@ class TestModels(TestCase):
                 'headers': {'Content-Type': 'application/json'},
                 'body': {'success': True},
             },
-        )
+        ),
+        (
+            {
+                'path': '/models/1234',
+                'httpMethod': 'GET',
+                'headers': {},
+                'queryStringParameters': {'locale': 'ja'},
+            },
+            {
+                'status': 200,
+                'headers': {'Content-Type': 'application/json'},
+                'body': {'success': True, 'id': 1234},
+            },
+        ),
     ])
     def test_action(self, event: dict, expected: dict):
         app = aws_app(event, object())
         actual = app.run().serialize()
         self.assertEqual(actual, expected)
+        unload_module(app.runner.__module__)
