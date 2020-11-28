f1a066e849f44cd5165a2df8e3b02ac43745a145
rog-works <rog_works@yahoo.co.jp>
2020-11-28 18:41:11 +0900

    test: changed to deserializer

--- tests/unit/framework/lang/test_serialize.py
+++ tests/unit/framework/lang/test_serialize.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass, field
+from enum import IntEnum
+from typing import Optional, Type
 from unittest import TestCase
 
-from framework.lang.serialize import DictSerializer
+from framework.lang.serialize import DictDeserializer, DictSerializer
+from tests.helper.fixture import data_provider
 
 
 @dataclass
@@ -34,3 +37,28 @@ class TestSerialize(TestCase):
             'f': [1, 2, 3],
         }
         self.assertEqual(serializer.serialize(data), expected)
+
+
+class EnumA(IntEnum):
+    A = 1
+    B = 2
+    C = 3
+
+
+class ClassA:
+    a: int = 0
+    b: str = ''
+    c: Optional[int] = None
+    e: EnumA = EnumA.A
+
+
+class TestDeserialize(TestCase):
+    @data_provider([
+        ({'a': 1, 'b': 'string', 'e': 3}, ClassA),
+        ({'a': 1, 'b': 'string', 'c': 2, 'e': 3}, ClassA),
+    ])
+    def test_dict_deserializer(self, data: dict, obj_type: Type):
+        deserializer = DictDeserializer()
+        actual = deserializer.deserialize(obj_type, data)
+        for key, value in data.items():
+            self.assertEqual(getattr(actual, key), value)
