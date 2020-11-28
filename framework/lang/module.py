34f17b88efe82842a10f111736833f83d1a4c915
rog-works <rog_works@yahoo.co.jp>
2020-11-28 17:55:50 +0900

    feat: add unload_module

--- framework/lang/module.py
+++ framework/lang/module.py
@@ -1,6 +1,14 @@
 from importlib import import_module
+import sys
 from typing import Any
 
 
 def load_module(path: str, module: str) -> Any:
     return getattr(import_module(path), module)
+
+
+def unload_module(path: str):
+    if path not in sys.modules:
+        raise ModuleNotFoundError()
+
+    del sys.modules[path]
