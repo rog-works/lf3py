e96971dfe727c440aae479eab92ccb5e809927d9
rog-works <rog_works@yahoo.co.jp>
2020-12-01 09:06:54 +0900

    refactor: update

--- framework/lang/annotation.py
+++ framework/lang/annotation.py
@@ -2,49 +2,6 @@ from enum import Enum
 from typing import Callable, Dict, List, Type, Union
 
 
-class FunctionAnnotation:
-    def __init__(self, func: Callable) -> None:
-        self._func = func
-
-    @property
-    def is_method(self) -> bool:
-        return hasattr(self._func, '__self__')
-
-    @property
-    def args(self) -> Dict[str, 'ArgAnnotation']:
-        if not hasattr(self._func, '__annotations__'):
-            return {}
-
-        return {key: ArgAnnotation(arg) for key, arg in self._func.__annotations__.items() if key != 'return'}
-
-    @property
-    def returns(self) -> 'ValueAnnotation':
-        if not hasattr(self._func, '__annotations__'):
-            return ValueAnnotation(type(None))
-
-        return ValueAnnotation(self._func.__annotations__['return'])
-
-
-class ClassAnnotation:
-    def __init__(self, klass: Type) -> None:
-        self._klass = klass
-
-    @property
-    def origin(self) -> Type:
-        return self._klass
-
-    @property
-    def constructor(self) -> FunctionAnnotation:
-        return FunctionAnnotation(self.origin.__init__)
-
-    @property
-    def properties(self) -> Dict[str, 'PropertyAnnotation']:
-        if not hasattr(self._klass, '__annotations__'):
-            return {}
-
-        return {key: PropertyAnnotation(prop) for key, prop in self._klass.__annotations__.items()}
-
-
 class ValueAnnotation:
     def __init__(self, prop: Type) -> None:
         self._prop = prop
@@ -88,3 +45,46 @@ class ValueAnnotation:
 
 PropertyAnnotation = ValueAnnotation
 ArgAnnotation = ValueAnnotation
+
+
+class FunctionAnnotation:
+    def __init__(self, func: Callable) -> None:
+        self._func = func
+
+    @property
+    def is_method(self) -> bool:
+        return hasattr(self._func, '__self__')
+
+    @property
+    def args(self) -> Dict[str, ArgAnnotation]:
+        if not hasattr(self._func, '__annotations__'):
+            return {}
+
+        return {key: ArgAnnotation(arg) for key, arg in self._func.__annotations__.items() if key != 'return'}
+
+    @property
+    def returns(self) -> ValueAnnotation:
+        if not hasattr(self._func, '__annotations__'):
+            return ValueAnnotation(type(None))
+
+        return ValueAnnotation(self._func.__annotations__['return'])
+
+
+class ClassAnnotation:
+    def __init__(self, klass: Type) -> None:
+        self._klass = klass
+
+    @property
+    def origin(self) -> Type:
+        return self._klass
+
+    @property
+    def constructor(self) -> FunctionAnnotation:
+        return FunctionAnnotation(self.origin.__init__)
+
+    @property
+    def properties(self) -> Dict[str, PropertyAnnotation]:
+        if not hasattr(self._klass, '__annotations__'):
+            return {}
+
+        return {key: PropertyAnnotation(prop) for key, prop in self._klass.__annotations__.items()}
