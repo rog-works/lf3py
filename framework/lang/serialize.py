f18adc26d66e1b6e33dee70daf1b3f938a1bad54
rog-works <rog_works@yahoo.co.jp>
2020-12-01 22:50:45 +0900

    fix: to any

--- framework/lang/serialize.py
+++ framework/lang/serialize.py
@@ -6,12 +6,12 @@ from framework.lang.annotation import ClassAnnotation, PropertyAnnotation
 
 class Serializer(metaclass=ABCMeta):
     @abstractmethod
-    def serialize(self, obj: Type) -> Any:
+    def serialize(self, obj: Any) -> Any:
         raise NotImplementedError()
 
 
 class DictSerializer(Serializer):
-    def serialize(self, obj: Type) -> dict:
+    def serialize(self, obj: Any) -> dict:
         return {key: value for key, value in obj.__dict__.items() if not key.startswith('_')}
 
 
