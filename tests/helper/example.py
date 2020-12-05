672da3618bbc04d02a7ae56e32d385e323d8337a
rog-works <rog_works@yahoo.co.jp>
2020-12-05 10:37:10 +0900

    test: fix to perform

--- tests/helper/example.py
+++ tests/helper/example.py
@@ -2,11 +2,12 @@ from example.provider import aws_app
 from example.router import routes
 from framework.api.router import Router
 from framework.lang.module import unload_module
+from framework.task.runner import Runner
 
 
 def perform_api(event: dict) -> dict:
     app = aws_app(event, object())
-    result = app.run().serialize()
+    result = app.perform(Runner).serialize()
     module_path, _ = Router(routes).resolve(app.api.request.method, app.api.request.path)
     unload_module(module_path)
     return result
