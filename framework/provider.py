c9f2445129599852172678ad31f9194c2dc76a14
rog-works <rog_works@yahoo.co.jp>
2020-12-10 22:33:00 +0900

    refactor: fix resolve symbol

--- framework/provider.py
+++ framework/provider.py
@@ -1,5 +1,5 @@
-from typing import Type, TypeVar, Union
-
+import re
+from typing import Dict, Type, TypeVar
 
 from framework.app import App
 from framework.lang.di import DI
@@ -8,15 +8,14 @@ from framework.lang.module import load_module_path
 _T = TypeVar('_T', bound=App)
 
 
-def app_provider(app_type: Type[_T], modules: Union[dict, list]) -> _T:
+def app_provider(app_type: Type[_T], modules: Dict[str, str]) -> _T:
     di = DI()
-    if type(modules) is dict:
-        for symbol_module_path, inject_module_path in modules.items():
+    for symbol_module_path, inject_module_path in modules.items():
+        if re.search(r'^[\w\d]+\.[\w\d]+', symbol_module_path):
             symbol = load_module_path(symbol_module_path)
             di.register(symbol, load_module_path(inject_module_path))
-    else:
-        for module_path in modules:
-            module = load_module_path(module_path)
-            di.register(module, load_module_path(module_path))
+        else:
+            module = load_module_path(inject_module_path)
+            di.register(module, module)
 
     return app_type(di)
