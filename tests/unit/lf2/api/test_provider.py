a461cbb7f76002c2e2961a5f37cfc3ec96ca4203
rog-works <rog_works@yahoo.co.jp>
2020-12-19 00:19:15 +0900

    test: add api/provider/request

--- tests/unit/lf2/api/test_provider.py
+++ tests/unit/lf2/api/test_provider.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from lf2.api.provider import api_router, bp_router, runner
+from lf2.api.provider import api_router, bp_router, request, runner
 from lf2.api.request import Request
 from lf2.api.route import BpRoute
 from lf2.aws.types import LambdaEvent
@@ -19,6 +19,16 @@ def action_2() -> Result:
 
 
 class TestProvider(TestCase):
+    def test_request(self):
+        event = LambdaEvent(
+            httpMethod='GET',
+            path='/',
+            headers={},
+            queryStringParameters={},
+        )
+        req = request(event)
+        self.assertEqual(type(req), Request)
+
     def test_bp_router(self):
         router = bp_router(Routes())
         self.assertEqual(type(router), Router)
