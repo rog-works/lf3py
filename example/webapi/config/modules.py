3d47c66ca1559b47d5cea9a9f3440868f939742e
rog-works <rog_works@yahoo.co.jp>
2020-12-19 15:08:25 +0900

    fix: dependency

--- example/webapi/config/modules.py
+++ example/webapi/config/modules.py
@@ -4,6 +4,7 @@ import os
 def modules() -> dict:
     return {
         'example.webapi.data.context.MyContext': 'example.webapi.provider.context.make_context',
+        'lf2.api.render.ApiRender': os.environ.get('MODULES_RENDER', 'lf2.api.render.ApiRender'),
         'lf2.api.request.Request': 'lf2.api.provider.request',
         'lf2.api.response.Response': 'example.webapi.provider.response.make_response',
         'lf2.api.errors.handler.ApiErrorHandler': 'lf2.api.errors.handler.ApiErrorHandler',
@@ -12,6 +13,5 @@ def modules() -> dict:
         'lf2.lang.cache.Cache': 'lf2.lang.cache.Cache',
         'lf2.i18n.i18n.I18n': 'example.webapi.provider.i18n.make_i18n',
         'lf2.task.types.Routes': 'example.webapi.config.routes.routes',
-        'lf2.view.render.IRender': os.environ.get('MODULES_RENDER', 'lf2.api.render.ApiRender'),
         'logging.Logger': 'example.webapi.provider.logger.dev_logger',
     }
--- lf2/app/webapp.py
+++ lf2/app/webapp.py
@@ -1,4 +1,5 @@
 from lf2.api.errors.handler import ApiErrorHandler
+from lf2.api.render import ApiRender
 from lf2.api.request import Request
 from lf2.api.routers.api import IApiRouter
 from lf2.app.app import App
@@ -6,7 +7,6 @@ from lf2.app.definitions import webapp_modules
 from lf2.aws.hooks.method import hook
 from lf2.aws.types import LambdaEvent
 from lf2.task.result import Result
-from lf2.view.render import IRender
 
 
 class WebApp(App):
@@ -19,8 +19,8 @@ class WebApp(App):
         return self._di.resolve(IApiRouter)
 
     @property
-    def render(self) -> IRender:
-        return self._di.resolve(IRender)
+    def render(self) -> ApiRender:
+        return self._di.resolve(ApiRender)
 
     @property
     def error(self) -> ApiErrorHandler:
