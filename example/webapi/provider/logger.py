23e3bbb05fbb22010564ed61acff1ffb3ffa5ee7
rog-works <rog_works@yahoo.co.jp>
2020-12-19 14:36:40 +0900

    fix: cleanup logger

--- example/webapi/provider/logger.py
+++ example/webapi/provider/logger.py
@@ -3,11 +3,13 @@ import logging
 from lf2.data.config import Config
 from lf2.i18n.i18n import I18n
 from lf2.logging.formatter import JsonFormatter
+from lf2.logging.cleaner import clear_handler
 
 
 def dev_logger(config: Config, i18n: I18n) -> logging.Logger:
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
+    clear_handler(logger)
 
     _config = config['logger']['dev_handler']
     handler = logging.FileHandler(_config['path'])
