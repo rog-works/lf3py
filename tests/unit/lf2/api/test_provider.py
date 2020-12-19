0e3570931a45adfc5d6dccb63d91a712be8f18d8
rog-works <rog_works@yahoo.co.jp>
2020-12-19 12:43:15 +0900

    test: fix

--- tests/unit/lf2/api/test_provider.py
+++ tests/unit/lf2/api/test_provider.py
@@ -1,13 +1,10 @@
 from unittest import TestCase
 
-from lf2.api.provider import api_router, bp_router, request, runner
+from lf2.api.provider import api_router, bp_router, request
 from lf2.api.request import Request
-from lf2.api.route import BpRoute
 from lf2.aws.types import LambdaEvent
 from lf2.task.result import Result
 from lf2.task.router import Router, Routes
-from lf2.task.runner import Runner
-from lf2.test.helper import data_provider
 
 
 def action_1() -> Result:
@@ -36,19 +33,3 @@ class TestProvider(TestCase):
     def test_ap_router(self):
         router = api_router()
         self.assertEqual(type(router), Router)
-
-    @data_provider([
-        ('GET', '/action/1', action_1),
-        ('GET', '/action/2', action_2),
-    ])
-    def test_runner(self, method: str, path: str, expected: Runner):
-        routes = Routes({
-            'GET /action/1': f'{__name__}.action_1',
-            'GET /action/2': f'{__name__}.action_2',
-        })
-        router = bp_router(routes)
-        event = LambdaEvent(httpMethod=method, path=path, headers={})
-        request = Request.from_event(event)
-        route = BpRoute(request, router)
-        actual = runner(request, route)
-        self.assertEqual(actual, expected)
--- tests/unit/lf2/api/test_response.py
+++ tests/unit/lf2/api/test_response.py
@@ -6,6 +6,6 @@ from lf2.api.response import Response
 class TestResponse(TestCase):
     def test_content_type(self):
         response = Response()
-        self.assertEqual(response.json().serialize(), {'status': 200, 'headers': {'Content-Type': 'application/json'}, 'body': {}})
-        self.assertEqual(response.html().serialize(), {'status': 200, 'headers': {'Content-Type': 'text/html'}, 'body': {}})
-        self.assertEqual(response.text().serialize(), {'status': 200, 'headers': {'Content-Type': 'text/plain'}, 'body': {}})
+        self.assertEqual(response.json().serialize(), {'statusCode': 200, 'headers': {'Content-Type': 'application/json'}, 'body': {}})
+        self.assertEqual(response.html().serialize(), {'statusCode': 200, 'headers': {'Content-Type': 'text/html'}, 'body': {}})
+        self.assertEqual(response.text().serialize(), {'statusCode': 200, 'headers': {'Content-Type': 'text/plain'}, 'body': {}})
