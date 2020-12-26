936c72983bfef749902866b9614ba7481bb36e75
rog-works <rog_works@yahoo.co.jp>
2020-12-26 23:33:57 +0900

    refactor: split error middleware

--- example/bpapi/api/users.py
+++ example/bpapi/api/users.py
@@ -1,32 +1,15 @@
-from typing import NoReturn
-
-from lf3py.api.errors import ApiError, BadRequestError, UnsupportedMediaTypeError
 from lf3py.api.response import Response
 from lf3py.middleware.api.cors import preflight_cors
 from lf3py.middleware.api.error import unexpected_dispach
 from lf3py.middleware.api.verifier import accept_json
-from lf3py.middleware.types import ErrorMiddlewares
 from lf3py.serialization.serializer import DictSerializer
 
 from example.bpapi.app import MyApp
 from example.bpapi.api.users_defs import IndexBody, ShowBody, CreateParams
+from example.bpapi.middleware.error import within
 from example.bpapi.models.user import User
 
 app = MyApp.get()
-
-
-def within(*statuses: int) -> ErrorMiddlewares:
-    def error_400(error: BadRequestError) -> NoReturn:
-        raise ApiError(app.i18n.trans('http.400'), 400) from error
-
-    def error_415(error: UnsupportedMediaTypeError) -> NoReturn:
-        raise ApiError(app.i18n.trans('http.415'), 415) from error
-
-    handlers = {
-        400: error_400,
-        415: error_415,
-    }
-    return tuple([handler for status, handler in handlers.items() if status in statuses])
 
 
 @app.behavior(preflight_cors)
---
+++ example/bpapi/middleware/error.py
@@ -0,0 +1,22 @@
+from typing import NoReturn
+
+from lf3py.api.errors import ApiError, BadRequestError, UnsupportedMediaTypeError
+from lf3py.middleware.types import ErrorMiddlewares
+
+from example.bpapi.app import MyApp
+
+
+def within(*statuses: int) -> ErrorMiddlewares:
+    app = MyApp.get()
+
+    def error_400(error: BadRequestError) -> NoReturn:
+        raise ApiError(app.i18n.trans('http.400'), 400) from error
+
+    def error_415(error: UnsupportedMediaTypeError) -> NoReturn:
+        raise ApiError(app.i18n.trans('http.415'), 415) from error
+
+    handlers = {
+        400: error_400,
+        415: error_415,
+    }
+    return tuple([handler for status, handler in handlers.items() if status in statuses])
