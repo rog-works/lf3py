16db6edf80a82e113546e2524aaecee29a0403a0
rog-works <rog_works@yahoo.co.jp>
2020-12-26 18:33:50 +0900

    bug: fix define callable

--- lf3py/middleware/types.py
+++ lf3py/middleware/types.py
@@ -2,5 +2,5 @@ from typing import Callable, NoReturn, Optional, Type, Tuple, Union
 
 DIRegister = Tuple[Type, Callable]
 Middleware = Callable[..., Union[NoReturn, Optional[DIRegister]]]
-ErrorMiddleware = Callable[[Exception]]
+ErrorMiddleware = Callable[[Exception], NoReturn]
 ErrorMiddlewares = Tuple[ErrorMiddleware, ...]
