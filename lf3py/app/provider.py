1c28e92bba36d3bfb024b28a274266e10a12bd8c
rog-works <rog_works@yahoo.co.jp>
2020-12-30 21:38:06 +0900

    style: remove unnecessary statements

--- lf3py/app/provider.py
+++ lf3py/app/provider.py
@@ -1,5 +1,3 @@
-import re
-
 from lf3py.config import ModuleDefinitions
 from lf3py.di import DI
 from lf3py.lang.module import load_module_path
@@ -8,11 +6,8 @@ from lf3py.lang.module import load_module_path
 def locator(modules: ModuleDefinitions) -> DI:
     di = DI()
     for symbol_module_path, inject_module_path in modules.items():
-        if re.search(r'^[\w\d]+\.[\w\d]+', symbol_module_path):
-            symbol = load_module_path(symbol_module_path)
-            di.register(symbol, load_module_path(inject_module_path))
-        else:
-            module = load_module_path(inject_module_path)
-            di.register(module, module)
+        symbol = load_module_path(symbol_module_path)
+        injector = load_module_path(inject_module_path)
+        di.register(symbol, injector)
 
     return di
