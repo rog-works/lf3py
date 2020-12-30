3599fc6f41506014e4228a462e07c301d821b1b9
rog-works <rog_works@yahoo.co.jp>
2020-12-30 21:43:09 +0900

    style: to add modules

--- example/sns/app.py
+++ example/sns/app.py
@@ -4,13 +4,14 @@ from lf3py.app.snsapp import SNSApp
 from lf3py.aws.symbols import IFireHose
 from lf3py.config.types import ModuleDefinitions
 
-from example.sns.modules import modules
+from example.sns.modules import add_modules
 
 
 class MyApp(SNSApp):
     @classmethod
     def module_definitions(cls) -> ModuleDefinitions:
-        return modules()
+        base = super(MyApp, cls).module_definitions()
+        return {**base, **add_modules()}
 
     @property
     def firehose(self) -> IFireHose:
