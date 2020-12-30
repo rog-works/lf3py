732426d8623be67c437984eaf5f01a819c177d81
rog-works <rog_works@yahoo.co.jp>
2020-12-30 21:25:31 +0900

    test: add serialization test data

--- tests/unit/lf3py/serialization/test_serializer.py
+++ tests/unit/lf3py/serialization/test_serializer.py
@@ -1,8 +1,15 @@
 from dataclasses import dataclass, field
+from enum import Enum
 from typing import Dict, List, Union
 from unittest import TestCase
 
 from lf3py.serialization.serializer import DictSerializer
+
+
+class AbcEnum(Enum):
+    A = 'a'
+    B = 'b'
+    C = 'c'
 
 
 @dataclass
@@ -14,6 +21,7 @@ class DataA:
     e: Dict[str, int] = field(default_factory=dict)
     f: List[int] = field(default_factory=list)
     g: Union[int, str] = 0
+    h: AbcEnum = AbcEnum.A
 
     @property
     def prop_a(self) -> str:
@@ -25,7 +33,7 @@ class DataA:
 
 class TestSerializer(TestCase):
     def test_dict_serializer(self):
-        data = DataA(10, 'hoge', True, 1.0, {'a': 1}, [1, 2, 3], 'fuga')
+        data = DataA(10, 'hoge', True, 1.0, {'a': 1}, [1, 2, 3], 'fuga', AbcEnum.C)
         serializer = DictSerializer()
         expected = {
             'a': 10,
@@ -35,5 +43,6 @@ class TestSerializer(TestCase):
             'e': {'a': 1},
             'f': [1, 2, 3],
             'g': 'fuga',
+            'h': 'c',
         }
         self.assertEqual(serializer.serialize(data), expected)
