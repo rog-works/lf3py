1a4c8bda4d405de607479d733d8cbf5daed8349c
rog-works <rog_works@yahoo.co.jp>
2021-01-01 12:38:28 +0900

    test: add wsgi handler

--- tests/unit/lf3py/wsgi/test_wsgi.py
+++ tests/unit/lf3py/wsgi/test_wsgi.py
@@ -1,9 +1,59 @@
 from io import BytesIO
 import json
+from typing import Any, Callable, List, Optional, Tuple
 from unittest import TestCase
 
 from lf3py.test.helper import data_provider
-from lf3py.wsgi.wsgi import RequestDecoder
+from lf3py.wsgi.wsgi import Handler, RequestDecoder
+
+
+class StubStartResponse:
+    def __init__(self) -> None:
+        self.status = ''
+        self.headers = []
+
+    def __call__(self, status: str, headers: List[Tuple[str, str]], exc_info: Optional[Any] = None) -> Callable[[bytes], Any]:
+        self.status = status
+        self.headers = headers
+        return lambda bytes: 1234
+
+
+class TestHandler(TestCase):
+    @data_provider([
+        (
+            {
+                'REQUEST_METHOD': 'GET',
+                'PATH_INFO': '/',
+                'CONTENT_TYPE': 'application/json',
+                'CONTENT_LENGTH': '',
+                'QUERY_STRING': 'lang=ja',
+                'wsgi.input': b'',
+            },
+            {
+                'status': '200 OK',
+                'headers': [('Content-Type', 'application/json')],
+                'body': [json.dumps({'success': True}).encode('utf-8')],
+            }
+        ),
+    ])
+    def test_call(self, environ: dict, expected: dict):
+        def lambda_handler(event: dict, context: object) -> dict:
+            return {
+                'statusCode': 200,
+                'headers': {
+                    'Content-Type': 'application/json',
+                },
+                'body': {
+                    'success': True,
+                },
+            }
+
+        handler = Handler(lambda_handler)
+        start_response = StubStartResponse()
+        body = handler(environ, start_response)
+        self.assertEqual(expected['status'], start_response.status)
+        self.assertEqual(expected['headers'], start_response.headers)
+        self.assertEqual(expected['body'], body)
 
 
 class TestRequestDecoder(TestCase):
