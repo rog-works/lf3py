4668ca2095e45799f5d46f9aa3115c8714dbae24
rog-works <rog_works@yahoo.co.jp>
2021-01-01 12:04:00 +0900

    style: split class

--- lf3py/wsgi/wsgi.py
+++ lf3py/wsgi/wsgi.py
@@ -15,17 +15,31 @@ class Handler:
         self._handler = handler
 
     def __call__(self, environ: dict, start_response: StartResponse) -> List[bytes]:
-        event = self.__decode_request(environ)
+        event = RequestDecoder.decode(environ)
         response = self._handler(event, object())
         status_desciption = self.__to_status_desciption(response['statusCode'])
         headers_tuple = [(key, value) for key, value in response['headers'].items()]
         start_response(status_desciption, headers_tuple)
         return [json.dumps(response['body']).encode('utf-8')]
 
-    def __decode_request(self, environ: dict) -> dict:
-        query_params = self.__parse_query_string(environ.get('QUERY_STRING', ''))
-        headers = self.__parse_headers(environ)
-        body = self.__parse_body(environ.get('wsgi.input', b''), headers['Content-Length'])
+    def __to_status_desciption(self, status: int) -> str:
+        mapping = {
+            200: '200 OK',
+            400: '400 Bad request',
+            401: '401 Unauthorized',
+            415: '415 Unsupported Media Type',
+            500: '500 Internal Server Error',
+            503: '503 Service Unavailable',
+        }
+        return mapping[status] if status in mapping else mapping[500]
+
+
+class RequestDecoder:
+    @classmethod
+    def decode(cls, environ: dict) -> dict:
+        query_params = cls.__parse_query_string(environ.get('QUERY_STRING', ''))
+        headers = cls.__parse_headers(environ)
+        body = cls.__parse_body(environ.get('wsgi.input', b''), headers['Content-Length'])
         return {
             'httpMethod': environ['REQUEST_METHOD'],
             'path': environ['PATH_INFO'],
@@ -34,7 +48,8 @@ class Handler:
             'body': body,
         }
 
-    def __parse_headers(self, environ: dict) -> Dict[str, Any]:
+    @classmethod
+    def __parse_headers(cls, environ: dict) -> Dict[str, Any]:
         headers = {
             'Content-Type': environ.get('CONTENT_TYPE', ''),
             'Content-Length': int(environ['CONTENT_LENGTH'] if environ.get('CONTENT_LENGTH', '') != '' else 0),
@@ -48,22 +63,13 @@ class Handler:
 
         return headers
 
-    def __parse_query_string(self, query_string: str) -> dict:
+    @classmethod
+    def __parse_query_string(cls, query_string: str) -> dict:
         return dict(parse_qsl(query_string))
 
-    def __parse_body(self, stream: TextIOWrapper, length: int) -> str:
+    @classmethod
+    def __parse_body(cls, stream: TextIOWrapper, length: int) -> str:
         return stream.read(length) if length > 0 else ''
-
-    def __to_status_desciption(self, status: int) -> str:
-        mapping = {
-            200: '200 OK',
-            400: '400 Bad request',
-            401: '401 Unauthorized',
-            415: '415 Unsupported Media Type',
-            500: '500 Internal Server Error',
-            503: '503 Service Unavailable',
-        }
-        return mapping[status] if status in mapping else mapping[500]
 
 
 class Server:
