0e9edf13e698937e1bfa6867a35bdcf3054f200e
rog-works <rog_works@yahoo.co.jp>
2021-01-03 18:25:07 +0900

    style: add abstract annotaion

--- lf3py/aws/symbols.py
+++ lf3py/aws/symbols.py
@@ -1,9 +1,11 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 
 
 class IFireHose(metaclass=ABCMeta):
+    @abstractmethod
     def __init__(self, delivery_stream_name: str) -> None:
         raise NotImplementedError()
 
+    @abstractmethod
     def put(self, payload: dict):
         raise NotImplementedError()
