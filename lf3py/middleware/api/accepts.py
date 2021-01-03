1d09be7e2ea5ad6fd082a42779143ae7f04f7fda
rog-works <rog_works@yahoo.co.jp>
2021-01-03 16:00:33 +0900

    feat: impl embed openapi schema

--- lf3py/middleware/api/accepts.py
+++ lf3py/middleware/api/accepts.py
@@ -1,7 +1,9 @@
 from lf3py.api.errors import NotAcceptableError
 from lf3py.api.request import Request
+from lf3py.openapi.schema import embed
 
 
+@embed.produce('application/json')
 def json(request: Request):
     if request.headers.get('Accept') != 'application/json':
         raise NotAcceptableError()
---
+++ lf3py/openapi/schema.py
@@ -0,0 +1,37 @@
+from typing import Any, Callable
+
+Func = Callable[..., Any]
+Decorator = Callable[[Func], Func]
+
+
+class Embed:
+    def produce(self, media_type: str) -> Decorator:
+        def decorator(func: Func) -> Func:
+            func.__openapi__ = {'produce': media_type}
+            return func
+
+        return decorator
+
+    def consume(self, media_type: str) -> Decorator:
+        def decorator(func: Func) -> Func:
+            func.__openapi__ = {'consume': media_type}
+            return func
+
+        return decorator
+
+    def security(self, security: dict) -> Decorator:
+        def decorator(func: Func) -> Func:
+            func.__openapi__ = {'security': security}
+            return func
+
+        return decorator
+
+    def responses(self, status: int, response: dict) -> Decorator:
+        def decorator(func: Func) -> Func:
+            func.__openapi__ = {'responses': {status: response}}
+            return func
+
+        return decorator
+
+
+embed = Embed()
